231dd270312dd73793d2450a4c49b02d84fd69a2
xdpiqbx <xdpiqbx@gmail.com>
2023-01-29 12:36:32 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Spring Security | FULL COURSE
+
+### [In process](https://youtu.be/her_7pa0vrg?t=1009)
---
+++ src/test/java/com/dpiqb/SpringSecurityCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpiqb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
